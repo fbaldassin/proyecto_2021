9dfcf63ea9732e1647ec8dad6cf9be0fac2f6400
fbaldassin <93685579+fbaldassin@users.noreply.github.com>
2021-11-15 21:15:46 -0300

    Add files via upload

---
+++ mi_proyecto_5.py
@@ -0,0 +1,11 @@
+narchivo = input('Ingresa un nombre de archivo: ')
+try:
+    man_a = open(narchivo)
+except:
+    print('No se puede abrir el archivo:', narchivo)
+    exit()
+contador = 0
+for linea in man_a:
+    if linea.startswith('Subject:'):
+        contador = contador + 1
+print('Hay', contador, 'lÃ­neas de asunto (subject) en', narchivo)
